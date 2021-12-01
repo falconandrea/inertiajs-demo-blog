87277bdd0101636559f648adf378854e351546e4
Andrea Falcon <falcon.andrea88@gmail.com>
2021-12-01 17:47:03 +0100

    feat: move posts as shared data

--- routes/web.php
+++ routes/web.php
@@ -3,26 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return inertia('Home', [
-        'posts' => [
-            [
-                'id' => 1,
-                'title' => "Title test 1",
-                'description' => "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est dignissimos qui perspiciatis dolorum iure, rem odit enim autem corporis, repellat neque optio. Quod, mollitia odio porro rem id dignissimos eius.",
-                'date' => "29 nov 2021"
-            ],
-            [
-                'id' => 2,
-                'title' => "Title test 2",
-                'description' => "Est dignissimos qui perspiciatis dolorum iure, rem odit enim autem corporis, repellat neque optio. Quod, mollitia odio porro rem id dignissimos eius.",
-                'date' => "28 nov 2021"
-            ],
-            [
-                'id' => 3,
-                'title' => "Title test 3",
-                'description' => "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est dignissimos qui perspiciatis dolorum iure, rem odit enim autem corporis, repellat neque optio. Quod, mollitia odio porro rem id dignissimos eius.",
-                'date' => "27 nov 2021"
-            ],
-        ]
-    ]);
+    return inertia('Home');
 });
