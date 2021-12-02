160184f2a10ad58cc60a82714ea5e3b5509c792d
Andrea Falcon <falcon.andrea88@gmail.com>
2021-12-02 15:55:32 +0100

    feat: add pagination

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -3,17 +3,19 @@
         <title>Home</title>
         <meta name="description" content="This is the Blog Homepage" head-key="description" />
     </Head>
-    <short-post v-for="post in posts" :key="post.id" :post="post"/>
+    <short-post v-for="post in posts.data" :key="post.id" :post="post"/>
+    <paginate :links="posts.links"/>
 </template>
 
 <script>
 import ShortPost from '../Shared/ShortPost.vue'
+import Paginate from '../Shared/Paginate.vue'
 export default {
     components: {
-        ShortPost
+        ShortPost, Paginate
     },
     props: {
-        posts: Array
+        posts: Object
     }
 }
 </script>
---
+++ resources/js/Shared/Paginate.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="mt-4 mb-8 text-center">
+        <template v-for="link in links" :key="link.label">
+            <Link :href="link.url" v-html="link.label" v-if="link.url" class="mx-4" />
+        </template>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        links: Array
+    }
+}
+</script>
