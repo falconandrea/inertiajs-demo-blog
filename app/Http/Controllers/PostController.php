47f566ce0aa4f5d57ae0c14d6bb6be21a0605cb9
Andrea Falcon <falcon.andrea88@gmail.com>
2021-12-02 16:20:30 +0100

    feat: add sarch in posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,13 +3,21 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use Illuminate\Support\Facades\Request;
 
 class PostController extends Controller
 {
     public function index()
     {
         return inertia('Home', [
-            'posts' => Post::latest()->paginate(3)
+            'posts' => Post::query()
+                ->when(Request::input('search'), function ($query, $search) {
+                    $query->where('title', 'like', '%' . $search . '%');
+                })
+                ->latest()
+                ->paginate(3)
+                ->withQueryString(),
+            'filters' => Request::only(['search'])
         ]);
     }
 
--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -3,11 +3,14 @@
         <title>Home</title>
         <meta name="description" content="This is the Blog Homepage" head-key="description" />
     </Head>
+    <input type="text" v-model="search" name="search" class="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Cerca nei post.." />
     <short-post v-for="post in posts.data" :key="post.id" :post="post"/>
     <paginate :links="posts.links"/>
 </template>
 
 <script>
+import { ref, watch } from 'vue'
+import { Inertia } from '@inertiajs/inertia'
 import ShortPost from '../Shared/ShortPost.vue'
 import Paginate from '../Shared/Paginate.vue'
 export default {
@@ -15,7 +18,21 @@ export default {
         ShortPost, Paginate
     },
     props: {
-        posts: Object
+        posts: Object,
+        filters: Object
+    },
+    data() {
+        return {
+            search: ref(this.$props.filters.search)
+        }
+    },
+    watch: {
+        search(newValue, oldValue) {
+            Inertia.get('/', { search: newValue }, {
+                preserveState: true,
+                replace: true
+            })
+        }
     }
 }
 </script>
