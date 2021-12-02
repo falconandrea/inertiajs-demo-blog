44d9b8e864ac03cb8f32ed99b5f8970edf22e1e6
Andrea Falcon <falcon.andrea88@gmail.com>
2021-12-02 16:00:03 +0100

    chore: add active link in pagination

--- resources/js/Shared/Paginate.vue
+++ resources/js/Shared/Paginate.vue
@@ -1,7 +1,12 @@
 <template>
     <div class="mt-4 mb-8 text-center">
         <template v-for="link in links" :key="link.label">
-            <Link :href="link.url" v-html="link.label" v-if="link.url" class="mx-4" />
+            <Link
+            :href="link.url"
+            v-html="link.label"
+            v-if="link.url"
+            class="mx-4"
+            :class="{ 'font-bold underline': link.active }" />
         </template>
     </div>
 </template>
